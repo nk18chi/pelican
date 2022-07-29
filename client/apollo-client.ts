43009acb610cb511d6a4f8958d8e09dfde01f7a2
Naoki Mita <naoki.mita18@gmail.com>
2022-07-28 19:15:13 -0700

    chore: support for codedeploy

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   link: createHttpLink({
-    uri: 'http://35.75.1.39:4000/graphql',
+    uri: 'http://localhost:4000/graphql',
   }),
   cache: new InMemoryCache(),
 });
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+sudo systemctl start docker
+sudo docker-compose -f /home/ec2-user/stripe-payment-app/docker-compose.yml down
+sudo docker-compose -f /home/ec2-user/stripe-payment-app/docker-compose.yml up -d --build

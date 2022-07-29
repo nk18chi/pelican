c2553fb6aae5e2104055b688bcd56ceef9c73303
Naoki Mita <naoki.mita18@gmail.com>
2022-07-28 21:32:09 -0700

    fix: revert fetch in apollo-client

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -1,8 +1,10 @@
+import fetch from 'node-fetch';
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   link: createHttpLink({
     uri: process.env.API_URL,
+    fetch: fetch,
   }),
   cache: new InMemoryCache(),
 });

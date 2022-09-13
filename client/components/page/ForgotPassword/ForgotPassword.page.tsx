c100ba349645c9cd810c62a6a667fcb5b17e7ebc
Naoki Mita <naoki.mita18@gmail.com>
2022-09-12 19:46:42 -0700

    refactor: organize file structure for ForgotPasswordPage

---
+++ client/components/page/ForgotPassword/ForgotPassword.page.tsx
@@ -0,0 +1,8 @@
+import { ForgotPasswordNextPageProps } from 'pages/forgot-password';
+import ForgotPassword from './ForgotPassword';
+
+const ForgotPasswordPage = (props: ForgotPasswordNextPageProps) => {
+  return <ForgotPassword {...props} />;
+};
+
+export default ForgotPasswordPage;

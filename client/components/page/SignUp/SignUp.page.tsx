ccfb7316ad925faa2ebcfcc4037165197009f412
Naoki Mita <naoki.mita18@gmail.com>
2022-09-19 19:54:46 -0700

    refactor: organize file structure for SignUp

---
+++ client/components/page/SignUp/SignUp.page.tsx
@@ -0,0 +1,8 @@
+import { SignUpNextPageProps } from 'pages/signup';
+import SignUp from './SignUp';
+
+const SignUpPage = (props: SignUpNextPageProps) => {
+  return <SignUp {...props} />;
+};
+
+export default SignUpPage;

5f83fe3dc439ef8d0ef99fd482049991958d26d5
Naoki Mita <naoki.mita18@gmail.com>
2022-09-28 21:23:09 -0700

    style: add Header.storybook

---
+++ client/components/shared/Header/Header.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import SidebarWithHeader from './SidebarWithHeader';
+
+export default {
+  title: 'Components/shared/Header',
+  component: SidebarWithHeader,
+  argTypes: {},
+} as ComponentMeta<typeof SidebarWithHeader>;
+
+const Template: ComponentStory<typeof SidebarWithHeader> = (args) => (
+  <SidebarWithHeader {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: <p>CHILDREN</p>,
+};

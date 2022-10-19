cb324d71e8021d7447a535e3a77ed53f7f9e976c
Naoki Mita <naoki.mita18@gmail.com>
2022-10-18 20:16:16 -0700

    refactor: move user profile under admin folder

--- client/components/page/UserProfile/UserProfile.page.tsx
+++ client/components/page/UserProfile/UserProfile.page.tsx
@@ -1,7 +1,7 @@
 import BannerCover from '@/components/layout/BannerCover';
 import { Footer } from '@/components/shared/Footer';
 import { SidebarWithHeader } from '@/components/shared/Header';
-import { UserProfileNextPageProps } from 'pages/user-profile';
+import { UserProfileNextPageProps } from 'pages/admin/user-profile';
 import UserProfile from './UserProfile';
 
 const UserProfilePage = (props: UserProfileNextPageProps) => {

290d01c2aed72604665cf9eef0eb952ae96a5a2b
Naoki Mita <naoki.mita18@gmail.com>
2022-11-08 16:47:01 -0800

    test: uncomment Header in Customer Detail

--- client/components/page/Admin/Customer/CustomerDetail.page.tsx
+++ client/components/page/Admin/Customer/CustomerDetail.page.tsx
@@ -1,17 +1,17 @@
 import BannerCover from '@/components/layout/BannerCover';
 // import { Footer } from '@/components/shared/Footer';
-// import { SidebarWithHeader } from '@/components/shared/Header';
+import { SidebarWithHeader } from '@/components/shared/Header';
 import { CustomerDetailNextPageProps } from 'pages/admin/customer/[customerId]';
 import CustomerDetail from './CustomerDetail';
 
 const CustomerDetailPage = (props: CustomerDetailNextPageProps) => {
   return (
     <>
-      {/* <SidebarWithHeader> */}
-      <BannerCover label="Customer Detail">
-        <CustomerDetail {...props} />
-      </BannerCover>
-      {/* </SidebarWithHeader> */}
+      <SidebarWithHeader>
+        <BannerCover label="Customer Detail">
+          <CustomerDetail {...props} />
+        </BannerCover>
+      </SidebarWithHeader>
       {/* <Footer /> */}
     </>
   );

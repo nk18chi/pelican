daca5a70b2db79284c8b2e3d2c7dab7b2e6889a3
Naoki Mita <naoki.mita18@gmail.com>
2022-12-28 21:37:04 -0800

    test: set queries/response in apollo client mock for cypress

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -2,16 +2,56 @@ import fetch from 'node-fetch';
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { MockLink, MockedResponse } from '@apollo/client/testing';
 import { GET_PRODUCT_FIND_MANY } from './gql/product';
+import { GET_TAX_FIND_MANY } from 'gql/tax';
+import { GET_PLAN_FIND_MANY } from 'gql/plan';
+import { GET_PLAN_OPTION_FIND_MANY } from 'gql/planOption';
 import productDummy from './cypress/fixtures/product.json';
+import taxDummy from './cypress/fixtures/tax.json';
+import planDummy from './cypress/fixtures/plan.json';
+import planOptionDummy from './cypress/fixtures/planOption.json';
 
 const mocks: MockedResponse[] = [
   {
     request: {
       query: GET_PRODUCT_FIND_MANY,
+      variables: { limit: 100 },
     },
     result: {
       data: {
         productFindMany: productDummy,
+      },
+    },
+  },
+  {
+    request: {
+      query: GET_TAX_FIND_MANY,
+      variables: { limit: 100 },
+    },
+    result: {
+      data: {
+        taxFindMany: taxDummy,
+      },
+    },
+  },
+  {
+    request: {
+      query: GET_PLAN_FIND_MANY,
+      variables: { limit: 100 },
+    },
+    result: {
+      data: {
+        planFindMany: planDummy,
+      },
+    },
+  },
+  {
+    request: {
+      query: GET_PLAN_OPTION_FIND_MANY,
+      variables: { limit: 100 },
+    },
+    result: {
+      data: {
+        planOptionFindMany: planOptionDummy,
       },
     },
   },

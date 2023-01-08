09347450a4dfb4f146e7c5e72fb6ab78a7128675
Naoki Mita <naoki.mita18@gmail.com>
2023-01-07 20:43:38 -0800

    feat: add password field in User

--- server/models/User.ts
+++ server/models/User.ts
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
     stripe: {},
